a38f2ab595d241a12748ffb02b96ac1e766a8bc2
Jhenzel Ponio <jhenzelgaliciaponio@gmail.com>
2025-01-15 22:13:32 +0800

    feat:

--- src/components/PlayerForm.jsx
+++ src/components/PlayerForm.jsx
@@ -22,7 +22,7 @@ const handleSubmit = async (e) => {
     }
 
     try{
-        const response = await fetch ("https://lolplayerform.azurewebsites.net/", {
+        const response = await fetch ("https://lolplayerform.azurewebsites.net/submit", {
             method:"POST",
             headers:{
                 "Content-Type": "application/json",
